516fec0c97967f3832dcc30a78f3978294f59e62
Yuri <rykov87yuri@gmail.com>
2025-06-09 11:17:38 +0300

    feat: add daily record form mode

--- src/entities/daily-record/ui/daily-record.form.tsx
+++ src/entities/daily-record/ui/daily-record.form.tsx
@@ -1,4 +1,4 @@
-import { Grid } from '@mui/material';
+import { Grid, ToggleButton, ToggleButtonGroup } from '@mui/material';
 import {
     FormContainer,
     FormContainerProps,
@@ -8,13 +8,27 @@ import { DatePickerElement } from 'react-hook-form-mui/date-pickers';
 import { DailyRecordValues } from '../model/daily-record.types';
 import { datePickerTransform } from 'shared/ui/input/date-picker.lib';
 import { inputNumberTransform } from 'shared/ui/input/input-number.lib';
+import { CheckOutlined, SportsBarOutlined } from '@mui/icons-material';
+import { MouseEvent, useState } from 'react';
+
+enum FormMode {
+    SPENDING = 'SPENDING',
+    NO_SPENDING = 'NO_SPENDING',
+}
 
 export const DailyRecordForm: React.FC<
     FormContainerProps<DailyRecordValues>
 > = (props) => {
+    const [mode, setMode] = useState<FormMode>(FormMode.NO_SPENDING);
+    const handleChangeMode = (
+        _: MouseEvent<HTMLElement>,
+        newMode: FormMode,
+    ) => {
+        setMode(newMode);
+    };
     return (
         <FormContainer {...props}>
-            <Grid container spacing={2}>
+            <Grid container spacing={3}>
                 <Grid size={12}>
                     <DatePickerElement
                         name="recordDate"
@@ -32,78 +46,103 @@ export const DailyRecordForm: React.FC<
                         transform={datePickerTransform}
                     />
                 </Grid>
-                <Grid size={4}>
-                    <TextFieldElement
-                        name="litersOfBeer"
-                        label="Выпито пива (литров)"
-                        fullWidth
-                        required
-                        rules={{
-                            required: true,
-                        }}
-                        transform={inputNumberTransform}
-                    />
+                <Grid size={12}>
+                    <ToggleButtonGroup
+                        value={mode}
+                        exclusive
+                        onChange={handleChangeMode}
+                    >
+                        <ToggleButton
+                            value={FormMode.NO_SPENDING}
+                            color="success"
+                        >
+                            <CheckOutlined /> Трат не было
+                        </ToggleButton>
+                        <ToggleButton value={FormMode.SPENDING} color="error">
+                            <SportsBarOutlined /> Траты были
+                        </ToggleButton>
+                    </ToggleButtonGroup>
                 </Grid>
-                <Grid size={4}>
-                    <TextFieldElement
-                        name="spentOnBeer"
-                        label="Потрачено на пиво (руб.)"
-                        fullWidth
-                        required
-                        rules={{
-                            required: true,
-                        }}
-                        transform={inputNumberTransform}
-                    />
-                </Grid>
-                <Grid size={4}>
-                    <TextFieldElement
-                        name="savedOnBeer"
-                        label="Сэкономлено на пиве (руб.)"
-                        fullWidth
-                        required
-                        rules={{
-                            required: true,
-                        }}
-                        transform={inputNumberTransform}
-                    />
-                </Grid>
-                <Grid size={4}>
-                    <TextFieldElement
-                        name="cigCount"
-                        label="Выкурино"
-                        fullWidth
-                        required
-                        rules={{
-                            required: true,
-                        }}
-                        transform={inputNumberTransform}
-                    />
-                </Grid>
-                <Grid size={4}>
-                    <TextFieldElement
-                        name="spentOnCig"
-                        label="Потрачено на сигареты (руб.)"
-                        fullWidth
-                        required
-                        rules={{
-                            required: true,
-                        }}
-                        transform={inputNumberTransform}
-                    />
-                </Grid>
-                <Grid size={4}>
-                    <TextFieldElement
-                        name="savedOnCig"
-                        label="Сэкономлено на сигаретах (руб.)"
-                        fullWidth
-                        required
-                        rules={{
-                            required: true,
-                        }}
-                        transform={inputNumberTransform}
-                    />
-                </Grid>
+                {mode === FormMode.SPENDING && (
+                    <>
+                        <Grid size={6}>
+                            <TextFieldElement
+                                name="litersOfBeer"
+                                label="Выпито пива (литров)"
+                                fullWidth
+                                required
+                                rules={{
+                                    required: true,
+                                }}
+                                transform={inputNumberTransform}
+                            />
+                        </Grid>
+                        <Grid size={6}>
+                            <TextFieldElement
+                                name="spentOnBeer"
+                                label="Потрачено на пиво (руб.)"
+                                fullWidth
+                                required
+                                rules={{
+                                    required: true,
+                                }}
+                                transform={inputNumberTransform}
+                            />
+                        </Grid>
+                        <Grid size={6}>
+                            <TextFieldElement
+                                name="cigCount"
+                                label="Выкурино"
+                                fullWidth
+                                required
+                                rules={{
+                                    required: true,
+                                }}
+                                transform={inputNumberTransform}
+                            />
+                        </Grid>
+                        <Grid size={6}>
+                            <TextFieldElement
+                                name="spentOnCig"
+                                label="Потрачено на сигареты (руб.)"
+                                fullWidth
+                                required
+                                rules={{
+                                    required: true,
+                                }}
+                                transform={inputNumberTransform}
+                            />
+                        </Grid>
+                    </>
+                )}
+                {mode === FormMode.NO_SPENDING && (
+                    <>
+                        <Grid size={6}>
+                            <TextFieldElement
+                                name="savedOnBeer"
+                                label="Сэкономлено на пиве (руб.)"
+                                fullWidth
+                                required
+                                rules={{
+                                    required: true,
+                                }}
+                                transform={inputNumberTransform}
+                            />
+                        </Grid>
+                        <Grid size={6}>
+                            <TextFieldElement
+                                name="savedOnCig"
+                                label="Сэкономлено на сигаретах (руб.)"
+                                fullWidth
+                                required
+                                rules={{
+                                    required: true,
+                                }}
+                                transform={inputNumberTransform}
+                            />
+                        </Grid>
+                    </>
+                )}
             </Grid>
         </FormContainer>
     );
